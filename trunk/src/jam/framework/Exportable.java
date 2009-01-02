84b6654c02afe7b8e0ab634ac9ca4f4db69c76d6
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2009-01-02 23:24:42 +0000

    Initial upload.

---
+++ trunk/src/jam/framework/Exportable.java
@@ -0,0 +1,21 @@
+/*
+ * Exportable.java
+ *
+ * Copyright (c) 2009 JAM Development Team
+ *
+ * This package is distributed under the Lesser Gnu Public Licence (LGPL)
+ *
+ */
+
+/**
+ * Exportable.java
+ */
+
+package jam.framework;
+
+import javax.swing.*;
+
+public interface Exportable {
+
+    JComponent getExportableComponent();
+}
