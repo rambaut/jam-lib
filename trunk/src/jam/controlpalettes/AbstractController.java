e5c56932517d675e7a9870e0000b3529a07dc7da
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2009-01-02 23:05:26 +0000

    Initial upload.

---
+++ trunk/src/jam/controlpalettes/AbstractController.java
@@ -0,0 +1,50 @@
+/*
+ * AbstractController.java
+ *
+ * Copyright (c) 2009 JAM Development Team
+ *
+ * This package is distributed under the Lesser Gnu Public Licence (LGPL)
+ *
+ */
+
+package jam.controlpalettes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Andrew Rambaut
+ * @author Alexei Drummond
+ * @version $Id: AbstractController.java 919 2008-06-09 23:54:44Z rambaut $
+ */
+public abstract class AbstractController implements Controller {
+	protected static final String IS_SHOWN = "isShown";
+
+    /**
+     * Add a ControllerListener to this controllers list of listeners
+     * The main listener will be the ControlPalette itself which will use
+     * this to resize the panels if the components changed
+     *
+     * @param listener
+     */
+    public void addControllerListener(ControllerListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Remove a listener
+     *
+     * @param listener
+     */
+    public void removeControllerListener(ControllerListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void fireControllerChanged() {
+        for (ControllerListener listener : listeners) {
+            listener.controlsChanged();
+        }
+    }
+
+    private final List<ControllerListener> listeners = new ArrayList<ControllerListener>();
+}
