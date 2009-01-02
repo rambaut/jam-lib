02003ca5ba331285a3fb38f37912bcac632de6e6
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2009-01-02 22:15:34 +0000

    Initial upload.

---
+++ trunk/src/jam/controlpalettes/ControllerListener.java
@@ -0,0 +1,19 @@
+/*
+ * ControllerListener.java
+ *
+ * Copyright (c) 2009 JAM Development Team
+ *
+ * This package is distributed under the Lesser Gnu Public Licence (LGPL)
+ *
+ */
+
+package jam.controlpalettes;
+
+/**
+ * @author Andrew Rambaut
+ * @author Alexei Drummond
+ * @version $Id: ControllerListener.java 363 2006-06-27 16:26:09Z rambaut $
+ */
+public interface ControllerListener {
+    void controlsChanged();
+}
