5e7c59defc6f8c414863fce801a752ad8257a5fc
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2009-01-02 22:24:59 +0000

    Initial upload.

---
+++ trunk/src/jam/demo/menus/DemoMenuHandler.java
@@ -0,0 +1,23 @@
+/*
+ * DemoMenuHandler.java
+ *
+ * Copyright (c) 2009 JAM Development Team
+ *
+ * This package is distributed under the Lesser Gnu Public Licence (LGPL)
+ *
+ */
+
+package jam.demo.menus;
+
+import javax.swing.*;
+
+/**
+ * @author Andrew Rambaut
+ * @version $Id: DemoMenuHandler.java 386 2006-07-17 21:35:49Z rambaut $
+ */
+public interface DemoMenuHandler {
+	Action getFirstAction();
+	Action getSecondAction();
+
+
+}
