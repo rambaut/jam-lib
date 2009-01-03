70cd76b478d093f34746499906395512d866ee80
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2009-01-03 00:19:16 +0000

    Initial upload.

--- trunk/src/jam/util/BrowserLauncher.java
+++ trunk/src/jam/util/BrowserLauncher.java
@@ -1,12 +1,3 @@
-/*
- * BrowserLauncher.java
- *
- * Copyright (c) 2009 JAM Development Team
- *
- * This package is distributed under the Lesser Gnu Public Licence (LGPL)
- *
- */
-
 package jam.util;
 
 import java.io.File;
--- trunk/src/jam/util/PrintUtilities.java
+++ trunk/src/jam/util/PrintUtilities.java
@@ -1,12 +1,3 @@
-/*
- * PrintUtilities.java
- *
- * Copyright (c) 2009 JAM Development Team
- *
- * This package is distributed under the Lesser Gnu Public Licence (LGPL)
- *
- */
-
 package jam.util;
 
 import javax.swing.*;
