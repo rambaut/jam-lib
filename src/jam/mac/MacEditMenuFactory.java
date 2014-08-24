c6f041f613c91ab2dae8bcf6f0d180a7561f3f82
rambaut <rambaut@1870b03d-ca4f-0410-83ed-ab9dbd7898a5>
2014-08-24 10:00:27 +0000

    Getting ConsoleApplication to work nicely with new OSXAdapter

--- src/jam/mac/MacEditMenuFactory.java
+++ src/jam/mac/MacEditMenuFactory.java
@@ -25,41 +25,41 @@ import java.awt.event.KeyEvent;
  * @version $Id: MacEditMenuFactory.java 964 2009-01-02 21:15:28Z rambaut $
  */
 public class MacEditMenuFactory implements MenuFactory {
-	public String getMenuName() {
-		return "Edit";
-	}
+    public String getMenuName() {
+        return "Edit";
+    }
 
-	public void populateMenu(JMenu menu, AbstractFrame frame) {
+    public void populateMenu(JMenu menu, AbstractFrame frame) {
 
-		JMenuItem item;
+        JMenuItem item;
 
         menu.setMnemonic('E');
 
         if (frame != null) {
-		item = new JMenuItem(frame.getCutAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getCutAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		item = new JMenuItem(frame.getCopyAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getCopyAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		item = new JMenuItem(frame.getPasteAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getPasteAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		item = new JMenuItem(frame.getDeleteAction());
-		menu.add(item);
+            item = new JMenuItem(frame.getDeleteAction());
+            menu.add(item);
 
-		item = new JMenuItem(frame.getSelectAllAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getSelectAllAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		menu.addSeparator();
+            menu.addSeparator();
 
-		item = new JMenuItem(frame.getFindAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getFindAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, MenuBarFactory.MENU_MASK));
+            menu.add(item);
         } else {
             item = new JMenuItem("Cut");
             item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, MenuBarFactory.MENU_MASK));
@@ -92,9 +92,9 @@ public class MacEditMenuFactory implements MenuFactory {
             item.setEnabled(false);
             menu.add(item);
         }
-	}
+    }
 
-	public int getPreferredAlignment() {
-		return LEFT;
-	}
+    public int getPreferredAlignment() {
+        return LEFT;
+    }
 }
--- src/jam/mac/MacFileMenuFactory.java
+++ src/jam/mac/MacFileMenuFactory.java
@@ -25,83 +25,106 @@ import java.awt.event.ActionEvent;
  */
 public class MacFileMenuFactory implements MenuFactory {
 
-	private final boolean isMultiDocument;
+    private final boolean isMultiDocument;
 
-	public MacFileMenuFactory(boolean isMultiDocument) {
-		this.isMultiDocument = isMultiDocument;
-	}
+    public MacFileMenuFactory(boolean isMultiDocument) {
+        this.isMultiDocument = isMultiDocument;
+    }
 
-	public String getMenuName() {
-		return "File";
-	}
+    public String getMenuName() {
+        return "File";
+    }
 
-	public void populateMenu(JMenu menu, AbstractFrame frame) {
+    public void populateMenu(JMenu menu, AbstractFrame frame) {
 
-		Application application = Application.getApplication();
-		JMenuItem item;
+        Application application = Application.getApplication();
+        JMenuItem item;
 
-		if (isMultiDocument) {
-			item = new JMenuItem(application.getNewAction());
-			item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, MenuBarFactory.MENU_MASK));
-			menu.add(item);
-		}
+        if (isMultiDocument) {
+            item = new JMenuItem(application.getNewAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, MenuBarFactory.MENU_MASK));
+            menu.add(item);
+        }
 
-		item = new JMenuItem(application.getOpenAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+        item = new JMenuItem(application.getOpenAction());
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, MenuBarFactory.MENU_MASK));
+        menu.add(item);
 
-		if (application.getRecentFileMenu() != null) {
-			JMenu subMenu = application.getRecentFileMenu();
-			menu.add(subMenu);
-		}
+        if (application.getRecentFileMenu() != null) {
+            JMenu subMenu = application.getRecentFileMenu();
+            menu.add(subMenu);
+        }
 
-		menu.addSeparator();
+        menu.addSeparator();
 
-		item = new JMenuItem(frame.getCloseWindowAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+        if (frame != null) {
+            item = new JMenuItem(frame.getCloseWindowAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		item = new JMenuItem(frame.getSaveAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getSaveAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK));
+            menu.add(item);
 
-		item = new JMenuItem(frame.getSaveAsAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK + ActionEvent.SHIFT_MASK));
-		menu.add(item);
+            item = new JMenuItem(frame.getSaveAsAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK + ActionEvent.SHIFT_MASK));
+            menu.add(item);
+        } else {
+            item = new JMenuItem("Close");
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, MenuBarFactory.MENU_MASK));
+            item.setEnabled(false);
+            menu.add(item);
 
-		item = new JMenuItem("Revert to Saved");
-		item.setEnabled(false);
-		menu.add(item);
+            item = new JMenuItem("Save");
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK));
+            item.setEnabled(false);
+            menu.add(item);
 
-		if (frame.getImportAction() != null || frame.getExportAction() != null) {
-			menu.addSeparator();
+            item = new JMenuItem("Save As...");
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, MenuBarFactory.MENU_MASK + ActionEvent.SHIFT_MASK));
+            item.setEnabled(false);
+            menu.add(item);
+        }
 
-			if (frame.getImportAction() != null) {
-				item = new JMenuItem(frame.getImportAction());
-				item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, MenuBarFactory.MENU_MASK));
-				menu.add(item);
-			}
+        item = new JMenuItem("Revert to Saved");
+        item.setEnabled(false);
+        menu.add(item);
 
-			if (frame.getExportAction() != null) {
-				item = new JMenuItem(frame.getExportAction());
-				item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, MenuBarFactory.MENU_MASK));
-				menu.add(item);
-			}
-		}
+        if (frame != null && (frame.getImportAction() != null || frame.getExportAction() != null)) {
+            menu.addSeparator();
 
-		menu.addSeparator();
+            if (frame.getImportAction() != null) {
+                item = new JMenuItem(frame.getImportAction());
+                item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, MenuBarFactory.MENU_MASK));
+                menu.add(item);
+            }
 
-		item = new JMenuItem(frame.getPrintAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, MenuBarFactory.MENU_MASK));
-		menu.add(item);
+            if (frame.getExportAction() != null) {
+                item = new JMenuItem(frame.getExportAction());
+                item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, MenuBarFactory.MENU_MASK));
+                menu.add(item);
+            }
+        }
 
-		item = new JMenuItem(application.getPageSetupAction());
-		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, MenuBarFactory.MENU_MASK + ActionEvent.SHIFT_MASK));
-		menu.add(item);
+        menu.addSeparator();
 
-	}
+        if (frame != null) {
+            item = new JMenuItem(frame.getPrintAction());
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, MenuBarFactory.MENU_MASK));
+            menu.add(item);
+        } else {
+            item = new JMenuItem("Print...");
+            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, MenuBarFactory.MENU_MASK));
+            item.setEnabled(false);
+            menu.add(item);
+        }
+        item = new JMenuItem(application.getPageSetupAction());
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, MenuBarFactory.MENU_MASK + ActionEvent.SHIFT_MASK));
+        menu.add(item);
 
-	public int getPreferredAlignment() {
-		return LEFT;
-	}
+    }
+
+    public int getPreferredAlignment() {
+        return LEFT;
+    }
 }
